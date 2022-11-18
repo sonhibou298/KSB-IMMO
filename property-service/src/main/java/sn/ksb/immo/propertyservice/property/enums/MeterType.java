e3fd84e7f1e1d3cb80e019169d9e49094cb33fe8
Marshall Dieme <58795947+marshall-dieme@users.noreply.github.com>
2022-11-18 14:40:41 +0000

    Delete property-service directory

--- property-service/src/main/java/sn/ksb/immo/propertyservice/property/enums/MeterType.java
+++
@@ -1,9 +0,0 @@
-package sn.ksb.immo.propertyservice.property.enums;
-
-public enum MeterType {
-    PUBLIC("Public"),
-    INDIVIDUAL("Individual");
-
-    MeterType(String name) {
-    }
-}
--- property-service/src/main/java/sn/ksb/immo/propertyservice/property/model/dto/RoomDTO.java
+++
@@ -1,9 +0,0 @@
-package sn.ksb.immo.propertyservice.property.model.dto;
-
-import lombok.Data;
-
-@Data
-public class RoomDTO {
-    private String propertyId;
-    private String type;
-}
